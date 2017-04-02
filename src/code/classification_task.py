2624d4f69ba9cba3e3816e885cfa2f62970d7bc5
ggaemo <jinwon@dm.snu.ac.kr>
2017-04-02 23:49:01 +0900

    added corruption variable modelling

--- src/code/classification_task.py
+++ src/code/classification_task.py
@@ -4,12 +4,36 @@ import pandas as pd
 import numpy as np
 import collections
 import tensorflow as tf
+import reader
+from reader import preprocess_for_onehot, preprocess_for_embedding, get_data_mushroom, input_producer
 
-from reader import preprocess_for_onehot, preprocess_for_embedding, get_data
+class InputMushroom(object):
+  """The input data."""
+  def __init__(self, batch_size=None):
+      input_data, target, unique_dict = reader.get_data_mushroom('onehot')
+      if batch_size:
+          self.input_data = reader.input_producer(input_data, batch_size)
+      else:
+          self.input_data = reader.input_producer(input_data, len(input_data))
+
+
+class InputTargetMushroom(object):
+    def __init__(self, batch_size):
+        input_data, target, unique_dict = reader.get_data_mushroom('onehot')
+        self.input_data, self.target = reader.input_target_producer(input_data, target, batch_size)
+
+class InputTargetMushroomAE():
+    def __init__(self, batch_size, data_path):
+        _, target, unique_dict = reader.get_data_mushroom('onehot')
+        input_data = np.load(data_path)
+        if batch_size:
+            self.input_data, self.target = reader.input_target_producer(input_data, target, batch_size)
+        else:
+            self.input_data, self.target = reader.input_target_producer(input_data, target, len(input_data))
 
 
 class Autoencoder():
-    def __init__(self, inputs, layer_dim, category_dim, squared_loss):
+    def __init__(self, inputs, layer_dim, is_training):
 
         input_dim = inputs.get_shape()[1]
         if not isinstance(layer_dim, collections.Iterable):
@@ -37,26 +61,28 @@ class Autoencoder():
                 layer_outputs.append(tf.add(tf.matmul(layer_outputs[-1],
                                                       tf.transpose(w_list[idx])), b))
 
-        if squared_loss:
-            self.loss = tf.reduce_mean(
-                tf.losses.mean_squared_error(inputs, layer_outputs[-1], scope='mse'))
-            tf.summary.scalar('mse', self.loss)
-        else:
-            for idx, cat_output_dim in enumerate(category_dim):
-                with tf.variable_scope('category_{}'.format(idx)):
-                    W = tf.get_variable('W', shape=[layer_dim[1], 1])
-                    b = tf.get_variable('b', shape=[layer_dim[idx + 1]],
-                                        initializer=tf.constant_initializer(0.0))
-                    layer_outputs.append(tf.add(tf.matmul(layer_outputs[-1],
-                                                          tf.transpose(w_list[idx])), b))
+
+        self.loss = tf.reduce_mean(
+            tf.losses.mean_squared_error(inputs, layer_outputs[-1], scope='mse'))
+        tf.summary.scalar('mse', self.loss)
+
+            # for idx, cat_output_dim in enumerate(category_dim):
+            #     with tf.variable_scope('category_{}'.format(idx)):
+            #         W = tf.get_variable('W', shape=[layer_dim[1], 1])
+            #         b = tf.get_variable('b', shape=[layer_dim[idx + 1]],
+            #                             initializer=tf.constant_initializer(0.0))
+            #         layer_outputs.append(tf.add(tf.matmul(layer_outputs[-1],
+            #                                               tf.transpose(w_list[idx])), b))
 
         self.layer_outputs = layer_outputs
 
-        self.global_step = tf.Variable(0, trainable=False, name='global_step')
-        self.train_op = tf.train.AdamOptimizer().minimize(self.loss, global_step=self.global_step)
-        self.init = tf.global_variables_initializer()
-        self.merged_summary = tf.summary.merge_all()
-        self.saver = tf.train.Saver()
+        # self.global_step = tf.Variable(0, trainable=False, name='global_step')
+        if is_training:
+            with tf.variable_scope('train'):
+                self.train_op = tf.train.AdamOptimizer().minimize(self.loss)
+        # self.init = tf.global_variables_initializer()
+        # self.merged_summary = tf.summary.merge_all()
+        # self.saver = tf.train.Saver()
 
 class Embedder():
     def __init__(self, inputs, embed_dim, input_dim):
@@ -87,105 +113,71 @@ class NeuralNetwork():
 
         self.loss = tf.losses.softmax_cross_entropy(target, layer_outputs[-1])
         self.global_step = tf.Variable(0, trainable=False, name ='global_step')
-        self.train_op = tf.train.AdamOptimizer().minimize(self.loss, global_step =self.global_step)
         self.prediction = tf.argmax(layer_outputs[-1], axis=1)
 
-        self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.prediction, tf.argmax(target,
-                                                                           axis=1)),
-                                tf.float32))
+        self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.prediction, tf.argmax(target,axis=1)),tf.float32))
+
+        self.train_op = tf.train.AdamOptimizer().minimize(self.loss, global_step=self.global_step)
         self.init = tf.global_variables_initializer()
 
 
-def train_autoencoder(max_epoch):
+def train_autoencoder(max_epoch, hidden_dim, batch_size, data):
+    print('training autoencoder on {}'.format(repr(data)))
+    log_dir = 'logdir_autoencoder_{}'.format(hidden_dim)
+    with tf.Graph().as_default():
+        # input_batch = InputMushroom(batch_size)
+        # input_batch_eval = InputMushroom()
+        input_batch = data(batch_size)
+        input_batch_eval = data()
+        with tf.variable_scope('Model', initializer=tf.truncated_normal_initializer(), reuse=None):
+            train_model = Autoencoder(input_batch.input_data, [hidden_dim],True)
 
-    input_data, target, unique_dict = get_data('onehot')
+        with tf.variable_scope('Model', initializer=tf.truncated_normal_initializer(), reuse=True):
+            eval_model = Autoencoder(input_batch_eval.input_data, [hidden_dim],False)
 
+        sv = tf.train.Supervisor(logdir=log_dir)
+        with sv.managed_session() as sess:
+            for i in range(max_epoch):
+                loss, _ = sess.run([train_model.loss, train_model.train_op])
 
-    hidden_dim = 500
-    writer = tf.summary.FileWriter('logdir_{}'.format(hidden_dim))
+                print('Epoch {} loss: {}'.format(i, loss))
 
+            hidden = sess.run(eval_model.hidden)
+            np.save(os.path.join(log_dir, 'autoencoder_representation.npy'.format(hidden_dim)), hidden)
 
-    with tf.Session() as sess:
-        input_placeholder = tf.placeholder(tf.float32, shape=[None, input_data.shape[1]])
-        with tf.variable_scope('Model', initializer=tf.truncated_normal_initializer()):
-            auto = Autoencoder(input_placeholder, [hidden_dim], [len(x) for x in
-                                                        unique_dict.values()],
-                               True)
-        sess.run(auto.init)
-        for i in range(max_epoch):
-            loss, _, summary = sess.run([auto.loss, auto.train_op, auto.merged_summary,
-                                         ], feed_dict={input_placeholder:input_data})
-
-            print('Epoch {} loss: {}'.format(i, loss))
-            writer.add_summary(summary, tf.train.global_step(sess, auto.global_step))
-
-        hidden = sess.run(auto.hidden, feed_dict={input_placeholder: input_data})
-
-        np.save('./logdir_{0}/autoencoder_representation.npy'.format(hidden_dim), hidden)
-        hidden_var = tf.Variable(hidden)
-        sess.run(tf.global_variables_initializer())
-
-        auto.saver.save(sess, 'logdir_{}/weights'.format(hidden_dim),
-                        global_step=tf.train.global_step(sess, auto.global_step))
-        writer.add_graph(graph=tf.get_default_graph())
-
-    writer.close()
-
-def train_classifier(max_epoch):
-    input_data, target, unique_dict = get_data('onehot')
-
-    input_data = np.load('./logdir_{0}/autoencoder_representation.npy'.format(
-        500))
-
-    with tf.Session() as sess:
-        input_placeholder = tf.placeholder(tf.float32, shape=[None, input_data.shape[1]])
-        target_placeholder = tf.placeholder(tf.int32, shape=[None, target.shape[1]])
-        net = NeuralNetwork(input_placeholder, target_placeholder, [5, 10, 10, target.shape[1]])
-        sess.run(net.init)
-
-        for i in range(max_epoch):
-            loss, _ = sess.run([net.loss, net.train_op], feed_dict={input_placeholder:
-                                                                        input_data,
-                                                                    target_placeholder:
-                                                                        target})
-            if i % 100 == 0:
-                acc = sess.run(net.accuracy, feed_dict={input_placeholder:
-                                                      input_data,
-                                                  target_placeholder:
-                                                      target})
-                print('Epoch : {0} loss: {1} accuracy : {2}'.format(i, loss, acc))
+            sv.saver.save(sess, os.path.join(log_dir, 'weights'.format(hidden_dim)),sv.global_step)
 
 
-def managed_train(max_epoch):
-    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data'
-    response = urllib.request.urlopen(url)
-    data = pd.read_csv(response, header=None)
+def train_classifier(max_epoch, layer_dim, batch_size, data):
+    print('training classifier on {}'.format(repr(data)))
+    log_dir = 'classifier_{}'.format(str(layer_dim))
 
     with tf.Graph().as_default():
-        input_data, unique_dict = preprocess_for_onehot(data)
-        producer = tf.train.input_producer(input_data).dequeue()
-        producer = tf.cast(producer, tf.float32)
+        # input_batch, target_batch = InputTargetMushroom(batch_size)
+        # input_batch_eval, target_batch_eval = InputTargetMushroom()
 
-        input_batch = tf.train.batch([producer], 32)
+        input_batch, target_batch = data(batch_size)
+        input_batch_eval, target_batch_eval = data()
+        layer_dim.append(target_batch.get_shape()[1]) #added for classification purposes
+        with tf.variable_scope('Classifier', initializer=tf.truncated_normal_initializer(), resuse=None):
+            net = NeuralNetwork(input_batch, target_batch, layer_dim)
 
-        input = tf.placeholder(tf.float32, shape=[None, input_data.shape[1]])
-        initializer = tf.truncated_normal_initializer()
-        with tf.variable_scope('Model', initializer=initializer):
-            auto = Autoencoder(input_batch, [500], [len(x) for x in unique_dict.values()],
-                               True)
+        with tf.variable_scope('Classifier', initializer=tf.truncated_normal_initializer(), resuse=True):
+            net_eval = NeuralNetwork(input_batch_eval, target_batch_eval, layer_dim)
+        sv = tf.train.Supervisor(logdir=log_dir)
 
-        sv = tf.train.Supervisor(logdir='logdir_sv')
         with sv.managed_session() as sess:
 
             for i in range(max_epoch):
-                loss, _ = sess.run([auto.loss, auto.train_op],
-                                    feed_dict={input:input_data})
-                print('Epoch {} loss: {}'.format(i, loss))
-
-            sv.saver.save(sess, 'save', global_step=sv.global_step)
+                sess.run([net.loss, net.train_op])
+                if i % 100 == 0:
+                    loss, acc = sess.run([net.loss, net.accuracy])
+                    eval_acc = sess.run(net_eval.accuracy)
+                    print('Epoch : {0} train loss: {1} eval accuracy'.format(i, loss, eval_acc))
+            sv.saver.save(sess, os.path.join(log_dir, 'weights'), global_step=sv.global_step)
 
 def embed(max_epoch):
-    input_data, target, unique_dict = get_data('embed')
+    input_data, target, unique_dict = get_data_mushroom('embed')
     hidden_dim = 500
     writer = tf.summary.FileWriter('logdir_{}'.format(hidden_dim))
 
@@ -218,7 +210,6 @@ def embed(max_epoch):
 
     writer.close()
 
-if __name__ == '__main__':
-    # train_classifier(1000)
-    # train(200)
-    managed_train(20000)
+
+
+
--- src/code/word2vec_basic.py
+++ src/code/word2vec_basic.py
@@ -11,55 +11,73 @@ import numpy as np
 import tensorflow as tf
 
 
-
-
-
-def maybe_download(filename, expected_bytes):
-  """Download a file if not present, and make sure it's the right size."""
-  if not os.path.exists(filename):
-    filename, _ = urllib.request.urlretrieve(url + filename, filename)
-  statinfo = os.stat(filename)
-  if statinfo.st_size == expected_bytes:
-    print('Found and verified', filename)
-  else:
-    print(statinfo.st_size)
-    raise Exception(
-        'Failed to verify ' + filename + '. Can you get to it with a browser?')
-  return filename
-
-
-def read_data(filename):
-  """Extract the first file enclosed in a zip file as a list of words"""
-  with zipfile.ZipFile(filename) as f:
-    data = tf.compat.as_str(f.read(f.namelist()[0])).split()
-  return data
-
-def build_dataset(words, vocabulary_size):
-    count = [['UNK', -1]]
-    count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
-    dictionary = dict()
-    for word, _ in count:
-        dictionary[word] = len(dictionary)
-
-    data = list()
-    unk_count = 0
-    for word in words:
-        if word in dictionary:
-            index = dictionary[word]
-        else:
-            index = 0 #dictionary['UNK']
-            unk_count += 1
-        data.append(index)
-    count[0][1] = unk_count
-    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
-    if not  os.path.exists('data.pkl'):
-        pickle.dump([data, count, dictionary, reverse_dictionary], open('data.pkl', 'wb'))
-    return data, count, dictionary, reverse_dictionary
-
-
-class Generator(object):
+class DataGenerator(object):
     def __init__(self):
         self.data_index = 0
+
+    def read_data(self, url='http://mattmahoney.net/dc/', filename='text8.zip'):
+
+        if not os.path.exists('data.pkl'):
+
+            def maybe_download(url, filename, expected_bytes):
+                """Download a file if not present, and make sure it's the right size."""
+                if not os.path.exists(filename):
+                    print('downloading file')
+                    filename, _ = urllib.request.urlretrieve(url + filename, filename)
+                statinfo = os.stat(filename)
+                if statinfo.st_size == expected_bytes:
+                    print('\tFound and verified', filename)
+                else:
+                    print(statinfo.st_size)
+                    raise Exception(
+                        'Failed to verify ' + filename + '. Can you get to it with a '
+                                                          'browser?')
+                return filename
+
+            filename = maybe_download(url, filename, 31344016)
+
+            print('Reading file')
+            """Extract the first file enclosed in a zip file as a list of words"""
+            with zipfile.ZipFile(filename) as f:
+                data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+                self.words = data
+            self.build_dataset()
+        else:
+            data, count, dictionary, reverse_dictionary = \
+                pickle.load(open('data.pkl', 'rb'))
+
+            self.data = data
+            self.count = count
+            self.dictionary = dictionary
+            self.reverse_dictionary = reverse_dictionary
+
+        print('loaded data, count,  dictionary, reverse dictionary')
+
+    def build_dataset(self, vocabulary_size = 50000):
+        count = [['UNK', -1]]
+        count.extend(collections.Counter(self.words).most_common(vocabulary_size - 1))
+        dictionary = dict()
+        for word, _ in count:
+            dictionary[word] = len(dictionary)
+
+        data = list()
+        unk_count = 0
+        for word in self.words:
+            if word in dictionary:
+                index = dictionary[word]
+            else:
+                index = 0  # dictionary['UNK']
+                unk_count += 1
+            data.append(index)
+        count[0][1] = unk_count
+        reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+        if not os.path.exists('data.pkl'):
+            pickle.dump([data, count, dictionary, reverse_dictionary],
+                        open('data.pkl', 'wb'))
+        self.data = data
+        self.count = count
+        self.dictionary,= dictionary
+        self.reverse_dictionary = reverse_dictionary
 
     def generate_batch(self, batch_size, num_skips, skip_window):
         '''
@@ -79,8 +97,8 @@ class Generator(object):
         span = 2 * skip_window + 1 #[skip_window target skip_window]
         buffer = collections.deque(maxlen=span)
         for _ in range(span):
-            buffer.append(data[self.data_index])
-            data_index = (self.data_index + 1) % len(data) # to cycle
+            buffer.append(self.data[self.data_index])
+            self.data_index = (self.data_index + 1) % len(self.data) # to cycle
         for i in range(batch_size // num_skips):
             target = skip_window # Just the initialized value
             targets_to_avoid = [skip_window]
@@ -90,37 +108,114 @@ class Generator(object):
                 targets_to_avoid.append(target)
                 batch[i*num_skips + j] = buffer[skip_window] #input is at the center
                 labels[i*num_skips + j] = buffer[target] # target is elsewhere
-            buffer.append(data[self.data_index]) # buffer pops the first element and adds a
+            buffer.append(self.data[self.data_index]) # buffer pops the first element and
+            # adds a
             #  new
             # element
-            data_index = (self.data_index + 1) % len(data)
+            self.data_index = (self.data_index + 1) % len(self.data)
         # Backtrack a little bit to avoid skipping words in the end of a batch
-        data_index = (self.data_index + len(data) - span) % len(data)
+        self.data_index = (self.data_index + len(self.data) - span) % len(self.data)
         return batch, labels
 
 
-
-# Step 1: Download the data.
-url = 'http://mattmahoney.net/dc/'
-
-filename = maybe_download('text8.zip', 31344016)
-
-# Step 2: Build the dictionary and replace rare words with UNK token.
-vocabulary_size = 50000
-if not os.path.exists('data.pkl'):
-    words = read_data(filename)
-    data, count, dictionary, reverse_dictionary = build_dataset(words, vocabulary_size)
-    del words
-else:
-    data, count, dictionary, reverse_dictionary = pickle.load(open('data.pkl', 'rb'))
-
-gen = Generator()
-batch, labels = gen.generate_batch(batch_size=8, num_skips=2, skip_window=1)
-for i in range(8):
-    print(batch[i], reverse_dictionary[batch[i]], '->', labels[i, 0],
-    reverse_dictionary[labels[i, 0]])
+    def sample(self):
+        print('showing samples')
+        batch, labels = self.generate_batch(8, 2, 1)
+        for i in range(8):
+            print(batch[i], self.reverse_dictionary[batch[i]], '->', labels[i, 0],
+                  self.reverse_dictionary[labels[i, 0]])
 
 
-print('Most common words (+UNK)', count[:5])
-print('Sample data', data[:10], [reverse_dictionary[i] for i in data[:10]])
+class Word2VecBasic():
+    def __init__(self, batch_size, embedding_size, skip_window, num_skips,
+                 vocabulary_size, num_sampled):
+        self.batch_size = batch_size
+        self.embedding_size = embedding_size
+        self.skip_window = skip_window
+        self.num_skips = num_skips
+        self.vocabulary_size = vocabulary_size
+        self.num_sampled = num_sampled
+
+
+    def build_model(self, inputs, train):
+
+        with tf.device('/cpu:0'):
+            embeddings = tf.get_variable('embedding', shape=[self.vocabulary_size],
+                                         initializer=tf.truncated_normal_initializer())
+            embed = tf.nn.embedding_lookup(embeddings, inputs.input_data)
+
+            nce_weights = tf.get_variable('nce_weights', shape=[self.vocabulary_size,
+                                                                self.embedding_size],
+                                          initializer=tf.truncated_normal_initializer())
+            nce_bias = tf.get_variable('nce_bias', shape=[self.vocabulary_size],
+                                       initializer=tf.constant_initializer())
+
+        loss = tf.reduce_mean(
+            tf.nn.nce_loss(nce_weights,
+                           nce_bias,
+                           inputs.targets,
+                           embed,
+                           self.num_sampled,
+                           self.vocabulary_size), name='nce_loss'
+        )
+
+        self.optimizer = tf.train.AdamOptimizer().minimize(loss)
+
+        if not train:
+            #cosine similarity check
+            norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+            normalized_embeddings = embeddings / norm
+            valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, inputs.input_data)
+            self.similarity = tf.matmul(valid_embeddings, normalized_embeddings,
+                                    transpose_b=True)
+
+
+def train(max_epoch):
+
+    gen = DataGenerator()
+    gen.read_data()
+
+    class example():
+        def __int__(self):
+            self.input_data = tf.placeholder(tf.int32, shape=[None])
+            self.targets = tf.placeholder(tf.int32, shape=[None, 1])
+
+
+    with tf.Graph().as_default():
+        train_data = example()
+        with tf.variable_scope('Train'):
+            model = Word2VecBasic(8, 64, 3, 2, 50000, 15)
+            model.build_model(train_data, True)
+        with tf.variable_scope('Valid', reuse=True):
+            valid_model = Word2VecBasic(8, 64, 3, 2, 50000, 15)
+            valid_model.build_model(train_data, False)
+
+        sv = tf.train.Supervisor(logdir='save')
+        with sv.managed_session() as sess:
+            for i in range(max_epoch):
+
+                input, label = gen.generate_batch(8, 3, 2)
+                sess.run(model.optimizer, feed_dict={train_data.input_data: input,
+                                                     train_data.targets: label})
+
+                if i % 10 == 0:
+                    sess.run(valid_model.similarity, )
+
+
+
+
+
+
+
+
+
+def test():
+    gen = DataGenerator()
+    gen.read_data()
+    gen.sample()
+    batch, labels = gen.generate_batch(batch_size=8, num_skips=2, skip_window=1)
+
+
+    print('Most common words (+UNK)', gen.count[:5])
+    print('Sample data', gen.data[:10], [gen.reverse_dictionary[i] for i in gen.data[:10]])
 
